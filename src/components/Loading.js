5e7180737ba34de9f659209a7dd09bce95889839
ashirbad <ashirbadbehera29@gmail.com>
2021-08-20 22:46:07 +0530

    🐛 Loading bug fix

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center space-x-2 animate-bounce">
+    <div
+      style={{ height: 'calc(100vh - 64px)' }}
+      className="flex items-center justify-center space-x-2 animate-bounce"
+    >
       <div className="w-6 h-6 bg-yellow-300 rounded-full"></div>
       <div className="w-6 h-6 bg-green-400 rounded-full"></div>
       <div className="w-6 h-6 bg-pink-300 rounded-full"></div>
