ce54210b88e969a0962bbf6e7eef20e1c643f590
ashirbad <ashirbadbehera29@gmail.com>
2021-08-20 22:24:00 +0530

    ⚡️ add products page

---
+++ src/components/Products/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/productList.scss';
+
+const Card = ({ val }) => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div className="m-4 relative overflow-hidden hover:shadow-sm">
+      <Link to={`/${val.id}`}>
+        <div
+          className="w-56"
+          onMouseEnter={() => setVisible(true)}
+          onMouseLeave={() => setVisible(false)}
+        >
+          <div className="w-56 h-64 overflow-hidden">
+            <img className="object-scale-down" src={val.img} alt={val.title} />
+          </div>
+          <div className="p-3 border relative">
+            <div className="leading-3">
+              <span className="text-lg font-medium truncate inline-block">
+                {val.title.slice(0, 11)}
+              </span>
+              <span className="text-sm inline-block">
+                {val.description.slice(0, 20)}
+              </span>
+            </div>
+            <span className="text-sm font-semibold">Rs. {val.price}</span>
+            <span className="ml-3 text-red-400 text-sm">( 40% OFF )</span>
+          </div>
+        </div>
+      </Link>
+      <div
+        className={`absolute w-56 hover-span py-5 px-2 border-r border-l ${
+          visible && 'hover-active'
+        }`}
+        onMouseEnter={() => setVisible(true)}
+        onMouseLeave={() => setVisible(false)}
+      >
+        <span
+          className={`w-full inline-block border text-center cursor-pointer py-1 font-medium hover:border-pink-300 `}
+          onClick={(e) => {}}
+        >
+          Wishlist
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/Products/ProductsList.js
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadProducts } from '../../redux/slices/productSlice';
+import Loading from '../Loading';
+
+import { Link } from 'react-router-dom';
+import Card from './Card';
+
+const category = ['kids war', 'mes war', 'electronics', 'chill'];
+
+const ProductsList = () => {
+  const { products, isLoading } = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadProducts());
+  }, [dispatch]);
+
+  if (isLoading) return <Loading />;
+
+  return (
+    <section className="h-screen">
+      <div className="w-5/6 mx-auto mt-10">
+        {category.map((val, idx) => (
+          <span
+            key={idx}
+            className="px-2 ml-4 mb-2 rounded-lg bg-red-100 inline-block cursor-pointer"
+          >
+            {val}
+          </span>
+        ))}
+      </div>
+      <div className="mx-auto flex flex-wrap w-5/6">
+        {products.map((val, idx) => {
+          return <Card val={val} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default ProductsList;
