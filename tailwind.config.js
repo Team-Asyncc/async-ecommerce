865a47e5499be5fa9bdedd2a510771bff8559184
Sukhseerat Kaur <karanjitkaur78@gmail.com>
2021-08-20 20:20:42 +0530

    updated hover and responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        lgCustom: '1100px',
+        xlCustom: '1340px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
