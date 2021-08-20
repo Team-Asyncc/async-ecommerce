071a7a9eaca873b2ffd1547bb03f11af4a7376c6
Arunsahu07 <is5617916@gmail.com>
2021-08-20 18:51:51 +0530

    added some more style

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,21 +1,23 @@
-import wishlist from "../assets/wishlist.png";
-import deliveryIcon from "../assets/delivery.png";
-import { useState, useEffect } from "react";
-import couponIcon from "../assets/coupon.png"
-import bagIcon from "../assets/bag.png";
-import "./cart.scss";
+import wishlist from '../assets/wishlist.png';
+import deliveryIcon from '../assets/delivery.png';
+import { useState, useEffect } from 'react';
+import couponIcon from '../assets/coupon.png';
+import bagIcon from '../assets/bag.png';
+import delIcon from '../assets/delete.png'
+import { useSelector } from 'react-redux';
+import './cart.scss';
 export default function Cart() {
   const [data, setData] = useState([]);
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
+    fetch('https://fakestoreapi.com/products')
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
 
-  const selectedItem = [];
+  const selectedItem = [1, 2, 3];
   let totalPrice = 0;
   return (
-    <div className="cart-container">
+    <div className="w-screen bg-white-500">
       <div className="cart-nav">My Bag</div>
       <div>
         {data.length > 0 ? (
@@ -28,87 +30,117 @@ export default function Cart() {
                   <span>Order...</span>
                 </div>
                 <div className="off-info">
-                  Apply coupon <span className="off-highlight">DEVSNEST10</span>{" "}
+                  Apply coupon <span className="off-highlight">DEVSNEST10</span>{' '}
                   to avail flat <span className="off-highlight">10%</span> off
-                  on your first order{" "}
+                  on your first order{' '}
                 </div>
-                <div className='devider'>
-                <section className="cart-item-field">
-                  {selectedItem.map((item, idx) => {
-                    totalPrice += data[item].price * 69;
-                    return (
-                      <div className="cart-item">
-                        <div className="cart-item-image-container">
-                          <img
-                            className="cart-item-image"
-                            src={data[item].image}
-                          />
-                        </div>
-                        <div>
-                        <div className="delivery-info">
-                            <img src={deliveryIcon} alt='Delivery' className="delivery-icon"/>
-                            <div>Delivery expected 30 august !<span className="free-tag">FREE</span></div>
+                <div className="devider">
+                  <section className="cart-item-field">
+                    {selectedItem.map((item, idx) => {
+                      totalPrice += data[item].price * 69;
+                      return (
+                        <div className="cart-item">
+                          <div className="cart-item-image-container">
+                            <img
+                              className="cart-item-image"
+                              src={data[item].image} alt="cartImage"
+                            />
                           </div>
-                          <div className="cart-item-title">
-                            {data[item].title}
-                          </div>
-                          
-                          <div className="cart-item-price">
-                            ₹ {parseInt(data[item].price * 69)}
-                            <span className="actual-price">
-                              {parseInt(data[item].price * 79)}
-                            </span>
-                            <div>Quantity : 1</div>
-                            <div className="wishlist-container">
-                              <img
-                                className="wishlist-icon"
-                                src={wishlist}
-                                als="wishlist"
-                              />
-                              <span>Save to wishlist</span>
+                            <img className='del-icon' src={delIcon} alt="del-Icon" />
+                          <div className >
+                            <div className="cart-item-title">
+                              {data[item].title}
+                            </div>
+
+                            <div className="cart-item-price">
+                              <span className="discounted-price">
+                                {' '}
+                                ₹ {(data[item].price * 69).toFixed(2)}
+                              </span>
+
+                              <span className="actual-price">
+                                {(data[item].price * 79).toFixed(2)}
+                              </span>
+
+                              <span className="off-span"> (10% off) </span>
+                              <div className="qnt-wl">
+                              <span><span>
+                                Quantity : <button>+</button>1<button>-</button></span></span>
+                              <div className="wishlist-container">
+                                <img
+                                  className="wishlist-icon"
+                                  src={wishlist}
+                                  alt="wishlist"
+                                />
+                              </div>
+                              </div>
+                              <div className="delivery-info">
+                                <img
+                                  src={deliveryIcon}
+                                  alt="Delivery"
+                                  className="delivery-icon"
+                                />
+                                <div>
+                                  Delivery expected 30 august !
+                                  <span className="free-tag">FREE</span>
+                                </div>
+                              </div>
                             </div>
                           </div>
                         </div>
+                      );
+                    })}
+                    <div className="continue-shoppings">Continue shopping</div>
+                  </section>
+                  <section className="cart-info">
+                    <div className="coupon-container">
+                      <img
+                        src={couponIcon}
+                        alt="coupun"
+                        className="coupon-icon"
+                      />
+                      <span>Apply for coupon</span>
+                      <input
+                        placeholder="apply"
+                        className="coupon-input"
+                      ></input>
+                    </div>
+                    <div className="cart-curr-status">
+                      <div className="shipping-info">
+                        Congratualtions !! your order is eligible for
+                        <span className="free-tag"> FREE </span> shipping!
                       </div>
-                    );
-                  })}
-                  <div className="continue-shoppings">Continue shopping</div>
-                </section>
-                <section className="cart-info">
-                  <div className="coupon-container">
-                    <img src={couponIcon} alt="coupun" className="coupon-icon" />
-                    <span >check for coupon</span>
-                    <input placeholder='apply' className='coupon-input' ></input>
-                  </div>
-                  <div className="cart-curr-status">
-                    <div className="shipping-info">Congratualtions !! your order is eligible for  
-                      <span className='free-tag'> FREE </span> shipping!
+                      <div className="bag-total">
+                        <span>Bag total</span>
+                        <span>{totalPrice.toFixed(2)}</span>
+                      </div>
+                      <div className="shipping-charge">
+                        <span>Shipping charge</span>
+                        <span className="free-tag">FREE</span>
+                      </div>
+                      <div className="bag-subtotal">
+                        <span>Bag subtotal</span>
+                        <span>{totalPrice.toFixed(2)}₹</span>
+                      </div>
+                      <div className="product-discount">
+                        <span>Product Discount(s)</span>
+                        <span>-{(totalPrice / 10).toFixed(2)}₹</span>
+                      </div>
+                      <div className="save-money">
+                        You will save {(totalPrice / 10).toFixed(2)}₹ on this
+                        order
+                      </div>
+                      <button className="text-white rounded-lg bg-pink-600 h-10 w-96 text-center margin text-xl">
+                        Place your order
+                      </button>
                     </div>
-                    <div className="bag-total">
-                      <span>Bag total</span><span>{totalPrice}</span>
-                    </div>
-                    <div className="shipping-charge">
-                      <span>Shipping charge</span><span className="free-tag">FREE</span>
-                    </div>
-                    <div className="bag-subtotal">
-                      <span>Bag subtotal</span><span>{totalPrice}₹</span>
-                    </div>
-                    <div className="product-discount">
-                      <span >Product Discount(s)</span><span>-{parseInt(totalPrice/10)}.00₹</span>
-                    </div>
-                    <div className="save-money">
-                      You will save {parseInt(totalPrice/10)}₹ on this order
-                    </div>
-
-                  </div>
-                </section>
+                  </section>
                 </div>
-
               </div>
             ) : (
               <div>
-                <div className="cart-item-container">
-                  <img src={bagIcon} alt="EmptyBag" className="empty-bag" />
+                <div className="cart-item-container flex justify-around items-center w-auto flex-col ">
+                  <img src={bagIcon} alt="EmptyBag" className="inline-block" />
                   <div className="cart-empty-msg">
                     Your bag is empty! Let’s fill it up shall we?
                   </div>
