9a92b1711f473d2ce75f09b481224c8338df7ab4
swag1223 <swaijot1201@gmail.com>
2021-08-23 16:46:18 +0530

    scroll-effect-added

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 
 import CategoryCard from './CategoryCard';
 
@@ -7,43 +8,67 @@ const Categories = () => {
     <div className="categories">
       <div className="text-4xl mb-10 ml-8 font-extrabold">CATEGORIES</div>
       <div className="bg-gradient-to-r from-pink-100 to-blue-100 bg-opacity-20 min-h-96 w-full rounded-xl grid grid-cols-1 sm:grid-cols-2  lgCustom:grid-cols-4 p-3">
-        <CategoryCard
+        <Link to="1" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-mens-clothing"
           category_type="Men's Clothing"
         />
+        </Link>
 
-        <CategoryCard
+        <Link to="0" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-womens-clothing"
           category_type="Women's Clothing"
         />
-
-        <CategoryCard
+        </Link>
+        
+        <Link to="2" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-kids-wear"
           category_type="Kids Wear"
         />
-        <CategoryCard
+        </Link>
+        
+        <Link to="3" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-footwear"
           category_type="Footwear"
         />
-
-        <CategoryCard
+          
+        </Link>
+        
+        <Link to="5" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-electronics"
           category_type="Electronics"
         />
 
-        <CategoryCard
+        </Link>
+        
+        <Link to="7" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-cosmetics"
           category_type="Cosmetics"
         />
-        <CategoryCard
+
+        </Link>
+        
+
+        <Link to="4" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-accessories"
           category_type="Accessories"
         />
-
-        <CategoryCard
+        </Link>
+        
+        <Link to="6" spy={true} smooth={true}>
+          <CategoryCard
           class_name="categories-home-decor"
           category_type="Home Decor"
-        />
+          />
+
+        </Link>
+        
       </div>
      
     </div>
--- src/components/LandingPageItems/Items.jsx
+++ src/components/LandingPageItems/Items.jsx
@@ -1,10 +1,10 @@
 import Container from './Container';
 
-const Items = ({ category }) => {
+const Items = ({ category , id}) => {
   return (
-    <div className="items mt-20">
+    <div id={id} className="items mt-20">
       <div className="heading mb-8  flex justify-between">
-        <div className="text-4xl ml-8  font-extrabold text-center inline-block">
+        <div  className="text-4xl ml-8  font-extrabold text-center inline-block">
           {category.heading}
         </div>
         <div className="mr-4 self-end">ViewMore</div>
